7e4eb1ff90570992ed2862112a864699a0090411
yuyuzeze <yuyuzeze@gmail.com>
2026-03-09 20:35:15 +0900

    build error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
